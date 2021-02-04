cb8fd1b1d43c296cd6d1959736d48d1c5ccc33ac
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-02-04 17:11:10 +0600

    True vs False

---
+++ trueFalse.js
@@ -0,0 +1,12 @@
+//falsy: false, 0, "", undefined, null, NaN
+
+//truthy: "0", " ", []
+
+const number = 0
+
+if (number){
+    console.log("this condition is true")
+}
+else{
+    console.log("this condition is false")
+}
