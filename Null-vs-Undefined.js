92dad6e95da82e2bd12da2d080865f54e3e70a72
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-02-05 15:49:07 +0600

    null vs undefined

---
+++ Null-vs-Undefined.js
@@ -0,0 +1,18 @@
+const lalu
+console.log(lalu);
+
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1*num2)
+    //konokichu return na korle undefined dekhabe
+}
+
+const romio = {name:"smart person", phone:3448339, address:"ny"};
+console.log(romio.age)
+
+let fun = undefined;
+console.log(fun);
